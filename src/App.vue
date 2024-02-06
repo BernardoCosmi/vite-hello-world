e9cd7110959c24fb8d5dd9b68936700323cc6793
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-06 17:21:03 +0100

    feat: jumbotron display from AppHero component. style: jumbotron alignment, title minor changes

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue'
-
+import AppHero from './components/AppHero.vue'
 </script>
 
 
 <template>
   <AppTitle/>
+  
+  <AppHero/>
 </template>
 
 <style scoped>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,30 @@
+<script>
+export default{
+    name: "AppHero"
+}
+</script>
+
+<template>
+
+    <div class="jumbotron">
+        <hr class="my-4">
+        <h2 class="display-4">Questo Jumbotron serve solo a richiamare la tua attenzione</h2>
+        <p class="lead">Questo serve solo a farti perdere altro tempo a leggere</p>
+        <hr class="my-4">
+    </div>
+    
+</template>
+
+<style scoped>
+    .jumbotron{
+        padding: 10px;
+        text-align: center;
+    }
+    h2, p, hr{
+        margin: 5px;
+    }
+    h2{
+      font-weight: 500;  
+      color: lightblue;
+    }
+</style>
