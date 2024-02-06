18750d30b37d6b3aa39290ec1ed653eba0f067ec
BernardoCosmi <bernardo.daddo90@gmail.com>
2024-02-06 16:49:58 +0100

    feat: print message from AppTitle component. fix: VUE template file removal

---
+++ src/components/AppTitle.vue
@@ -0,0 +1,22 @@
+<script>
+export default{
+    name: "AppTitle"
+}
+</script>
+
+<template>
+
+  <div>
+    <h1 class="green">Hello World!</h1>
+  </div>
+
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 700;
+}
+div, h1{
+    text-align: center;
+}
+</style>
